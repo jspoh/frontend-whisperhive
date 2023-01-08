6093878bc71efc98a61d30b6c2181b01c410cccf
jspoh <jingsengyeah@gmail.com>
2023-01-08 21:55:17 +0800

    added subscription to call observable making http request

--- projects/social-media/src/app/features/profile/profile.component.ts
+++ projects/social-media/src/app/features/profile/profile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DataService } from '../../services/data.service';
+import { map, takeUntil, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-profile',
@@ -10,14 +11,24 @@ import { DataService } from '../../services/data.service';
 export class ProfileComponent implements OnInit {
   userData: any;
 
+  unsubscribe$ = new Subject();
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private dataService: DataService
   ) {}
 
   ngOnInit(): void {
-    this.userData = this.dataService.getUser(
-      this.activatedRoute.snapshot.url[0].path
-    );
+    this.dataService
+      .getUser(this.activatedRoute.snapshot.url[0].path)
+      .pipe(
+        map((response: any) => (this.userData = response)),
+        takeUntil(this.unsubscribe$)
+      )
+      .subscribe({
+        next(res) {},
+        error(err) {},
+        complete() {},
+      });
   }
 }

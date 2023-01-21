68cd8279866237cfd47e50c421897fc29ca5db61
jspoh <jingsengyeah@gmail.com>
2023-01-21 23:17:12 +0800

    added post-card component to user route but routing breaks on that page

--- projects/social-media/src/app/features/profile/profile.module.ts
+++ projects/social-media/src/app/features/profile/profile.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
-
+import { PostCardModule } from '../../shared/components/post-card/post-card.module';
 
 @NgModule({
-  declarations: [
-    ProfileComponent
-  ],
-  imports: [
-    CommonModule,
-    ProfileRoutingModule
-  ]
+  declarations: [ProfileComponent],
+  imports: [CommonModule, ProfileRoutingModule, PostCardModule],
 })
-export class ProfileModule { }
+export class ProfileModule {}

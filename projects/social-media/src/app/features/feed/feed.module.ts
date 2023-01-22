68446c58a46d7b439cc26093b91bd0d860a7ebeb
jspoh <jingsengyeah@gmail.com>
2023-01-22 20:32:31 +0800

    pls dont break lol. updated angular to v15

--- projects/social-media/src/app/features/feed/feed.module.ts
+++ projects/social-media/src/app/features/feed/feed.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { FeedRoutingModule } from './feed-routing.module';
 import { FeedComponent } from './feed.component';
 import { PostCardModule } from '../../shared/components/post-card/post-card.module';
+// import { QuillModule } from 'ngx-quill';
 
 @NgModule({
   declarations: [FeedComponent],
-  imports: [CommonModule, FeedRoutingModule, PostCardModule],
+  imports: [
+    CommonModule,
+    FeedRoutingModule,
+    PostCardModule,
+    // QuillModule.forRoot(),
+  ],
 })
 export class FeedModule {}

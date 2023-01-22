ce8ee1400f97cab6380665c872a608ca9e509eae
jspoh <jingsengyeah@gmail.com>
2023-01-22 16:07:24 +0800

    created large profile component

--- projects/social-media/src/app/shared/components/profile-card/profile-card.component.ts
+++ projects/social-media/src/app/shared/components/profile-card/profile-card.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from '../../../models/user';
 
 @Component({
   selector: 'app-profile-card',
   templateUrl: './profile-card.component.html',
-  styleUrls: ['./profile-card.component.scss']
+  styleUrls: ['./profile-card.component.scss'],
 })
 export class ProfileCardComponent implements OnInit {
+  @Input() userData: User = {
+    username: '',
+    data: { followingList: [], followerList: [], posts: [] },
+  };
 
-  constructor() { }
+  /**
+   * This input only takes in 'lg', 'md', 'sm'
+   */
+  @Input() cardSize = 'lg';
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }

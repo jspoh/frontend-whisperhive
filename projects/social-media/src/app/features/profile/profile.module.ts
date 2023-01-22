f028a13d2b1d4b2021bd0a1d93c269dc3fe50513
jspoh <jingsengyeah@gmail.com>
2023-01-22 11:08:34 +0800

    updated stylings to make post card component more reusable

--- projects/social-media/src/app/features/profile/profile.module.ts
+++ projects/social-media/src/app/features/profile/profile.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
 import { PostCardModule } from '../../shared/components/post-card/post-card.module';
+import { ProfileCardModule } from '../../shared/components/profile-card/profile-card.module';
 
 @NgModule({
   declarations: [ProfileComponent],
-  imports: [CommonModule, ProfileRoutingModule, PostCardModule],
+  imports: [
+    CommonModule,
+    ProfileRoutingModule,
+    PostCardModule,
+    ProfileCardModule,
+  ],
 })
 export class ProfileModule {}
